1e2a43c5815e771525c8febd055d021f8bb37d11
PavlykIryna <155089011+PavlykIryna@users.noreply.github.com>
2024-04-28 15:24:48 +0200

    finish-04

--- js/task-2.js
+++ js/task-2.js
@@ -11,7 +11,7 @@ class Storage{
         removeItem(itemToRemove){
             const index = this.items.indexOf(itemToRemove);
             if (index !== -1) {
-                this.items.slice(index, 1);
+                this.items.splice(index, 1);
                 }
         }
         }

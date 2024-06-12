26c2e1244409139d71ed023bdf9ecd4ee01f206e
PavlykIryna <155089011+PavlykIryna@users.noreply.github.com>
2024-06-13 01:38:40 +0200

    finish

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,18 @@
-class Storage{
-    constructor(initialItems = []) {
-        this.items = initialItems;
+class Storage {
+    #items;
+    constructor(arrItems = []) {
+        this.#items = arrItems; 
     }
-    getItems(){
-        return this.items;
-    }
+    getItems() {
+        return this.#items;
+    };
     addItem(newItem) {
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
-        removeItem(itemToRemove){
-            const index = this.items.indexOf(itemToRemove);
-            if (index !== -1) {
-                this.items.splice(index, 1);
-                }
-        }
-        }
-
-    
-    
-    
-    
-    
-    
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
